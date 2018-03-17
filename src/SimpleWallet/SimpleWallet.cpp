8c8836c5992e2a3f6406f6a87bde377550e5cfe6
Brondo76 <35544655+Brondo76@users.noreply.github.com>
2018-03-17 17:01:02 +0100

    Update SimpleWallet.cpp

--- src/SimpleWallet/SimpleWallet.cpp
+++ src/SimpleWallet/SimpleWallet.cpp
@@ -519,8 +519,8 @@ bool processServerAliasResponse(const std::string& s, std::string& address) {
 	try {
 
 		// Courtesy of Monero Project
-		// make sure the txt record has "oa1:krb" and find it
-		auto pos = s.find("oa1:krb");
+		// make sure the txt record has "oa1:sdi" and find it
+		auto pos = s.find("oa1:sdi");
 		if (pos == std::string::npos)
 			return false;
 		// search from there to find "recipient_address="

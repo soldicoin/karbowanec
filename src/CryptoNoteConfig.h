79f7e547b3d4a734eb9eaf0afedccd442fa4d73d
BitSoldiAdmin <35544655+BitSoldiAdmin@users.noreply.github.com>
2018-04-27 22:11:53 +0200

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -103,8 +103,8 @@ const size_t   BLOCKS_IDS_SYNCHRONIZING_DEFAULT_COUNT        =  10000;  //by def
 const size_t   BLOCKS_SYNCHRONIZING_DEFAULT_COUNT            =  128;    //by default, blocks count in blocks downloading
 const size_t   COMMAND_RPC_GET_BLOCKS_FAST_MAX_COUNT         =  1000;
 
-const int      P2P_DEFAULT_PORT                              =  31280;
-const int      RPC_DEFAULT_PORT                              =  31281;
+const int      P2P_DEFAULT_PORT                              =  8280;
+const int      RPC_DEFAULT_PORT                              =  8281;
 
 const size_t   P2P_LOCAL_WHITE_PEERLIST_LIMIT                =  1000;
 const size_t   P2P_LOCAL_GRAY_PEERLIST_LIMIT                 =  5000;

fe636cbfaf9772b3b5e71215bf153c119427b4ce
BitSoldiAdmin <35544655+BitSoldiAdmin@users.noreply.github.com>
2018-04-29 01:46:20 +0200

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -128,10 +128,10 @@ const uint32_t P2P_IDLE_CONNECTION_KILL_INTERVAL             = (5 * 60);      //
 const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "3a96y6a8u964i8p1640o55336858dobuio8c668pe635poe365u8i84d085pa210";
 
 const char* const SEED_NODES[] = { 
-  "212.237.7.129:31280",
-  "94.177.187.216:31280",
-  "80.211.225.136:31280",
-  "212.237.29.222:31280"
+  "212.237.7.129:8280",
+  "94.177.187.216:8280",
+  "80.211.225.136:8280",
+  "212.237.29.222:8280"
 };
 
 struct CheckpointData {
